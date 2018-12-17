c0c88629bcc86e7a4e40de38d799a1a4d300361b
Jahziel Villasana <jahzielv@gmail.com>
2018-12-16 22:19:34 -0500

    basic footer created

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,32 @@
 import React from "react";
+import { withStyles, Typography } from "@material-ui/core";
+import { height } from "window-size";
 
-class Footer extends React.Component {
-    constructor(props) {
-        super(props);
+const styles = theme => (
+    {
+        footer: {
+            position: "absolute",
+            left: "0",
+            bottom: "0",
+            right: "0",
+            height: "35px"
+        },
+        footerText: {
+            paddingRight: "20px",
+            // paddingBottom: "10px",
+            color: "navy"
+        }
     }
+);
 
-    render() {
-        return (
-            <div style={{ color: "blue" }}>Yeet</div>
-        );
-    }
+
+
+const Footer = ({ classes }) => {
+    return (
+        <div className={classes.footer}>
+            <Typography className={classes.footerText} align="right">Made with &#10084; by <a href="https://jahz.co">JVE</a>.</Typography>
+        </div>
+    );
 }
 
-export default Footer;
+export default withStyles(styles)(Footer);
