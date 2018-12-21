520800a1853db9ebbcdc739b89e5e03b5c366703
Jahziel Villasana <jahzielv@gmail.com>
2018-12-21 15:45:35 -0500

    updated readme

--- src/components/InputArea.js
+++ src/components/InputArea.js
@@ -1,20 +1,14 @@
-import React from 'react';
-import {
-    Fab,
-    Grid,
-    TextField,
-    Snackbar
-} from '@material-ui/core';
+import React from "react";
+import { Fab, Grid, TextField, Snackbar } from "@material-ui/core";
 import fetch from "node-fetch";
 const saveAs = require("file-saver");
 
 export class InputArea extends React.Component {
-
     state = {
         id: "",
         portErr: false,
-        badUrl: false
-    }
+        badUrl: false,
+    };
 
     deckButtonClick = () => {
         let qUrl = null;
@@ -23,44 +17,54 @@ export class InputArea extends React.Component {
             if (qUrl.hostname !== "quizlet.com") {
                 throw TypeError;
             }
-            let qId = qUrl.pathname.split("/")[1]
+            let qId = qUrl.pathname.split("/")[1];
             let filename;
             fetch(`https://ankiport-api.appspot.com/port?setID=${qId}`)
-
-                .then((response) => {
+                .then(response => {
                     filename = response.headers.get("x-filename");
                     if (filename === null) {
-                        return Promise.reject("404 error - invalid set ID number");
+                        return Promise.reject(
+                            "404 error - invalid set ID number"
+                        );
                     } else {
                         return response.blob();
                     }
                 })
-                .then((blob) => saveAs(blob, filename))
-                .catch((err) => {
+                .then(blob => saveAs(blob, filename))
+                .catch(err => {
                     console.log(err);
                     this.setState({ portErr: true });
                 });
         } catch (TypeError) {
             this.setState({ badUrl: true });
         }
-
-    }
+    };
 
     handleClose = () => {
         this.setState({ portErr: false });
-    }
+    };
 
     handleCloseUrl = () => {
         this.setState({ badUrl: false });
-    }
-
+    };
 
     render() {
         const { portErr, badUrl } = this.state;
         return (
-            <Grid container spacing={40} style={{ paddingTop: "40px" }} direction="column" >
-
-                <Grid container item direction="row" justify='flex-start' alignItems="center" spacing={40}>
+            <Grid
+                container
+                spacing={40}
+                style={{ paddingTop: "40px" }}
+                direction="column"
+            >
+                <Grid
+                    container
+                    item
+                    direction="row"
+                    justify="flex-start"
+                    alignItems="center"
+                    spacing={40}
+                >
                     <TextField
                         id="url-field"
                         label="Quizlet Deck URL"
@@ -71,11 +75,16 @@ export class InputArea extends React.Component {
                         style={{ width: "1000px" }}
                         onChange={e => {
                             this.setState({ id: e.target.value });
-
                         }}
                     />
                     <div style={{ paddingLeft: "30px" }}>
-                        <Fab id="buttonPort" variant="extended" color="primary" size="large" onClick={this.deckButtonClick}>
+                        <Fab
+                            id="buttonPort"
+                            variant="extended"
+                            color="primary"
+                            size="large"
+                            onClick={this.deckButtonClick}
+                        >
                             Make a deck!
                         </Fab>
                     </div>
@@ -86,9 +95,17 @@ export class InputArea extends React.Component {
                     autoHideDuration={6000}
                     onClose={this.handleClose}
                     ContentProps={{
-                        'aria-describedby': 'porterr-msg',
+                        "aria-describedby": "porterr-msg",
                     }}
-                    message={<span id="porterr-msg">We couldn't find that Quizlet <span role="img" aria-label="disappointed-face">😔</span>Please try again!</span>}
+                    message={
+                        <span id="porterr-msg">
+                            We couldn't find that Quizlet{" "}
+                            <span role="img" aria-label="disappointed-face">
+                                😔
+                            </span>
+                            Please try again!
+                        </span>
+                    }
                 />
                 <Snackbar
                     anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
@@ -96,14 +113,20 @@ export class InputArea extends React.Component {
                     autoHideDuration={6000}
                     onClose={this.handleCloseUrl}
                     ContentProps={{
-                        'aria-describedby': 'message-id',
+                        "aria-describedby": "message-id",
                     }}
-                    message={<span id="message-id">Make sure your URL is correct <span role="img" aria-label="eyes">👀</span></span>}
+                    message={
+                        <span id="message-id">
+                            Make sure your URL is correct{" "}
+                            <span role="img" aria-label="eyes">
+                                👀
+                            </span>
+                        </span>
+                    }
                 />
-            </Grid >
+            </Grid>
         );
     }
 }
-
 
 export default InputArea;
